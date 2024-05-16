32ef5464e8feee29d37a09903e488019e7a9887e
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2024-05-16 15:31:27 +0100

    folder set up and initial comments for venv set up - repo remuade as error of missing index and pycache couldnt be resolved

---
+++ app.py
@@ -0,0 +1,20 @@
+#at the end of project pip free all to requirements so they can be loaded by next user#####
+
+# Web Application in FLASK
+# Enviroment set up on MAC: 
+# cd "to project folder via terminal"
+# python3 -m venv .venv
+# . .venv/bin/activate
+# pip install Flask
+
+# Enviroment set up on Windows: 
+# mkdir myproject
+# cd myproject
+# py -3 -m venv .venv
+# .venv\Scripts\activate
+# pip install Flask
+
+#to run:    export FLASK_APP=wsaaproject
+#           flask run
+#           ctrl+c to exit flask
+
